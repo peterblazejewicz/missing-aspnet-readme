28d1e879d0c4f313d52f8d7f6d9040a43f60f24d
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-05-03 22:58:34 +0200

    :art: Add converter for https protocol
    This will convert all MS short links
    to use https protocol

--- scripts/index.js
+++ scripts/index.js
@@ -38,11 +38,21 @@ request({
 				return content;
 			}
 		};
+        var protocolConverter = {
+            filter: ["a"],
+            replacement: function(content, node) {
+                var url = node.href;
+                if(url) url = url.replace(/http:\/\/go\.microsoft\.com/i, 'https://go.microsoft.com');
+                node.href = url;
+                console.log(node.href + " " + content);
+                return content;
+            }
+        };
 		console.log("done");
 		console.log("parsing content to Github flavoured Markdown ...");
 		var md = toMarkdown($body.html(), {
 			gfm: true,
-			converters: [contentConverter]
+			converters: [contentConverter, protocolConverter]
 		});
 		console.log("updating README.md");
 		fs.writeFile("../README.md", md, function (error) {
