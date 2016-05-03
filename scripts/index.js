8241e5417a1d76e87c9fb294ed697c8f71a19e9e
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-05-03 23:22:27 +0200

    Code formatting

--- scripts/index.js
+++ scripts/index.js
@@ -23,51 +23,51 @@ var fs = require("fs");
 var url = pkg.configuration.url;
 console.log("fetching content from " + url);
 request({
-	uri: url,
-	headers: {
-		"User-Agent": "The missing README.md for ASP.NET 5 @peterblazejewicz"
-	}
+    uri: url,
+    headers: {
+        "User-Agent": "The missing README.md for ASP.NET 5 @peterblazejewicz"
+    }
 }, function (error, response, body) {
-	if (response.statusCode === 200) {
-		console.log("parsing content with cheerio");
-		var $ = cheerio.load(body);
+    if (response.statusCode === 200) {
+        console.log("parsing content with cheerio");
+        var $ = cheerio.load(body);
         $("a[href^='http://go.microsoft.com']")
-            .each(function() {
-                if(this.attribs.href) {
-                    this.attribs.href =  this.attribs.href
+            .each(function () {
+                if (this.attribs.href) {
+                    this.attribs.href = this.attribs.href
                         .replace(/http:\/\/go\.microsoft\.com/i, "https://go.microsoft.com");
                 }
             });
-		var $body = $("body");
-		var contentConverter = {
-			filter: ["div", "span"],
-			replacement: function (content) {
-				return content;
-			}
-		};
-		console.log("done");
-		console.log("parsing content to Github flavoured Markdown ...");
-		var md = toMarkdown($body.html(), {
-			gfm: true,
-			converters: [contentConverter]
-		});
-		console.log("updating README.md");
-		fs.writeFile("../README.md", md, function (error) {
-			if (error) {
-				console.log(error);
-				return;
-			}
-			console.log("README.md updated");
-		});
-	}
+        var $body = $("body");
+        var contentConverter = {
+            filter: ["div", "span"],
+            replacement: function (content) {
+                return content;
+            }
+        };
+        console.log("done");
+        console.log("parsing content to Github flavoured Markdown ...");
+        var md = toMarkdown($body.html(), {
+            gfm: true,
+            converters: [contentConverter]
+        });
+        console.log("updating README.md");
+        fs.writeFile("../README.md", md, function (error) {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            console.log("README.md updated");
+        });
+    }
 
 }).on("data", function (data) {
-	console.log("data stream received");
+    console.log("data stream received");
 }).on("response", function (response) {
-	console.log("server status: " + response.statusCode);
-	response.on("data", function (data) {
-		console.log("data stream receiving...");
-	});
+    console.log("server status: " + response.statusCode);
+    response.on("data", function (data) {
+        console.log("data stream receiving...");
+    });
 }).on("error", function (error) {
-	console.log("The request failed" + error);
+    console.log("The request failed" + error);
 });
